1cf851eb42d19985df347e773bea6a1d3b2a0430
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-01-19 10:05:37 +0000

    Improved instructions and command line messages

--- doBoth.sh
+++ doBoth.sh
@@ -1,3 +1,26 @@
-collateClip $1 $2 $3 $4
-blockVary $3 10000 $4
-showAll.sh $3
+#!/bin/bash
+
+#Check correct number of command line arguments
+if (( $# < 4 )); then
+    echo "Presents sqWell output data" >&2
+    echo "Runs the following: " >&2
+    echo " -CollateClip: averages over block data, clipping out early results if desired." >&2
+    echo " - blockVary: computes the upper and low range explored during a block">&2
+    echo "Plots results via xmgrace"
+    echo "" > &2
+   echo "Usage  :  doBoth [int matchPoint][int Maximum energy][input filename][int averaging start point]" >&2
+   echo "Example:  doBoth.sh 0 100 inputFiles/N10TestRun.dat 1" >&2
+  exit 1
+fi
+
+#====Extract arguments====
+matchPoint=$1
+MaxEnergy=$2
+inputFile=$3
+aveStart=$4
+
+
+
+collateClip $matchPoint $MaxEnergy $inputFile $aveStart
+blockVary $inputFile 10000 $aveStart
+showAll.sh $inputFile
