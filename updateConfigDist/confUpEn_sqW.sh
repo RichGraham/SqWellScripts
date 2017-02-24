fa4b45ddcb5961c752fa114d6e8c972427b8ee25
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-02-24 11:38:13 +0000

    Added an argument to specify the minimum temperature

--- updateConfigDist/confUpEn_sqW.sh
+++ updateConfigDist/confUpEn_sqW.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 #Check correct number of command line arguments
-if (( $# < 3 )); then
-    echo "Computes P(nc|E) for a specific E for temps 0....HighestTemp" >&2
+if (( $# < 4 )); then
+    echo "Computes P(nc|E) for a specific E for temps LowestTemp....HighestTemp and plots dists" >&2
     echo "Runs the following: " >&2
     echo " - updateConfigDist_sqW: takes all config files in a folder and updates the current distribution" >&2
     echo " ">&2
-   echo "Usage  :  confUpEn_sqW.sh [base folder][int EnergyVal][int HighestTemp]" >&2
-   echo "Example:  confUpEn_sqW.sh outputFiles/N40_OpOut1/ 100 12" >&2
+   echo "Usage  :  confUpEn_sqW.sh [base folder][int EnergyVal][int LowestTemp][int HighestTemp]" >&2
+   echo "Example:  confUpEn_sqW.sh outputFiles/N40_OpOut1/ 100 0 11g" >&2
   exit 1
 fi
 
 #====Extract arguments====
 BaseFolder=$1
 EnergyVal=$2
-HighestTemp=$3
+LowestTemp=$3
+HighestTemp=$4
 
 cd $BaseFolder/configs
 
-for i in $(seq 0 $HighestTemp)
+for i in $(seq $LowestTemp $HighestTemp)
 do
     cd T$i/E$EnergyVal
     pwd
