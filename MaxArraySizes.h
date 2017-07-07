3521f241210d75ee4b51fc888be2ba4a2fbd04cd
RichGraham <Richard.Graham@nottingham.ac.uk>
2017-07-07 09:14:27 +0100

    Larger Arrays

--- MaxArraySizes.h
+++ MaxArraySizes.h
@@ -1,3 +1,3 @@
-#define MAX_TEMPS 31 //The maximum number of temperatures used for parallel tempering. Should be at least 1 larger than the required value.
+#define MAX_TEMPS 40 //The maximum number of temperatures used for parallel tempering. Should be at least 1 larger than the required value.
 #define MAX_NSQ 2500 // Maximum number of allowed square well neighbours
 #define MAX_LENGTH 1800 // Maximum number of allowed beads on the chain
